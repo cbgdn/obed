13bc4152df5401e34a7685a6a40d832c2c7e17f9
Art4 <art4@wlabs.de>
2020-06-28 16:05:22 +0200

    index.html in pages/index.vue verschoben

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,10 +4,6 @@
 	</div>
 </template>
 
-<!-- <script src="../node_modules/popper.js/dist/popper.min.js"></script> -->
-<!-- <script src="../node_modules/jquery/dist/jquery.js"></script> -->
-<!-- <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script> -->
-
 <style lang="css">
   @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 </style>
